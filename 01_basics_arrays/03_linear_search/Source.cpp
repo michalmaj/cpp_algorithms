62db41dcdbcd65fe4ea6e17616464098203ae496
Michał Maj <michalpmaj@gmail.com>
2022-08-30 17:14:39 +0200

    linear search

---
+++ 01_basics_arrays/03_linear_search/Source.cpp
@@ -0,0 +1,31 @@
+/*
+ * This is example of brute-force algorithm.
+ * Searching for a given value in an array one by one.
+ */
+#include <iostream>
+
+// Generic function for linear search.
+template <class T, size_t N>
+int linear_search(T (&arr)[N], T key)
+{
+	// If element doesn't exist in an array, index will be -1.
+	int idx{ -1 };
+
+	for(int i{0}; i < std::size(arr); ++i)
+	{
+		if (arr[i] == key)
+			idx = i;
+	}
+
+	return idx;
+}
+
+int main()
+{
+	int arr[]{ 1, 2, 0, 10, 11, 9, 8, 8, 7 };
+	constexpr int key{ 9 };
+	auto result = linear_search(arr, key);
+	std::cout << "Index of looking item is: " << result << std::endl;
+
+	return 0;
+}
