e63ab43264f1a0d91668d6e4e67e51b1e0448948
Michał Maj <michalpmaj@gmail.com>
2022-09-04 16:38:35 +0200

    selection sort

---
+++ 02_sorting/03_selection_sort/Source.cpp
@@ -0,0 +1,13 @@
+/*
+ * Selection sort
+ * Repeatedly find the minimum element in unsorted part of the array.
+ * Then put this element at the beginning.
+ */
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
