d6571ec8fb57dc98b7ce41ed4247e3c06744112b
Michał Maj <michalpmaj@gmail.com>
2022-09-09 18:14:19 +0200

    2D array basics

--- 04_2D_arrays/01_2D_array_basics/Source.cpp
+++ 04_2D_arrays/01_2D_array_basics/Source.cpp
@@ -1,3 +1,20 @@
+/*
+ * There are two ways to store 2D array, because  in memory everything must be linearized.
+ * So two common ways of storing the arrays:
+ *		1) Row Major Form.
+ *		2) Column Major Form.
+ * For example, we have 2D array (3 rows x 4 columns):
+ *
+ *		{
+ *			{8, 6, 5, 4},
+ *			{2, 1, 9, 7},
+ *			{3, 6, 4, 2}
+ *		}
+ *	In Row-Major will be stored as:
+ *		8,6,5,5,2,1,9,7,3,6,4,2
+ *	In Column-Major will be stored as:
+ *		8,2,3,6,1,6,5,9,4,4,7,2 
+ */
 #include <iostream>
 
 template <size_t N, size_t M>
