54e884f1bed3a545f135b172ae65b164f76be390
Michał Maj <michalpmaj@gmail.com>
2022-09-19 16:11:14 +0200

    even or odd

---
+++ 07_bit_manipulation/01_odd_even/Source.cpp
@@ -0,0 +1,33 @@
+/*
+ * In C++, bitwise operators perform operations on integer data at the individual bit-level.
+ * These operations include testing, setting, or shifting the actual bits.
+ *
+ *		Operator		Description
+ *			&		Bitwise AND Operator
+ *			|		Bitwise OR Operator
+ *			^		Bitwise XOR Operator
+ *			~		Bitwise Complement Operator
+ *			<<		Bitwise Shift Left Operator
+ *			>>		Bitwise Shift Right Operator
+ */
+
+#include <iostream>
+
+int main()
+{
+	// Bitwise check if number is even or odd. Any number in binary is odd if last
+	// bit is 1. Otherwise the number will be even. For example:
+	// 00000001 = 1; 000000010 = 2; 000000011 = 3; 000000100 = 4
+	// So to check if number is even or odd we can use & operator with 1
+
+	int x;
+	std::cout << "Enter the number to check if is even or odd: ";
+	std::cin >> x;
+
+	if (x & 1)
+		std::cout << "Number is odd\n";
+	else
+		std::cout << "Number is even\n";
+
+	return 0;
+}
