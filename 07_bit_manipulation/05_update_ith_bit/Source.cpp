7a5119f7b03cb3068384117114121176b073c639
Michał Maj <michalpmaj@gmail.com>
2022-09-24 15:28:15 +0200

    update ith bit

--- 07_bit_manipulation/05_update_ith_bit/Source.cpp
+++ 07_bit_manipulation/05_update_ith_bit/Source.cpp
@@ -10,7 +10,7 @@
  */
 void clearIthBit(int& n, int i)
 {
-	// Set bit to 1 at given index, rest will be 0's, then switch 1 to 0 and 0' to 1's
+	// Set bit to 1 at given index, rest will be 0's, then switch 1 to 0 and 0' to 1's.
 	int mask{ ~(1 << i) };
 
 	std::cout << std::setw(20) << "Mask: " << std::setw(10) << std::bitset<8>(mask) << std::endl;
