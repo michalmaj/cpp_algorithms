200b609fd6dee63369bcf91ca0b046fa762192e7
Michał Maj <michalpmaj@gmail.com>
2022-09-24 17:36:07 +0200

    clear last i bits

--- 07_bit_manipulation/06_clear_last_i_bits/Source.cpp
+++ 07_bit_manipulation/06_clear_last_i_bits/Source.cpp
@@ -13,10 +13,13 @@ void clearLastIBits(int& n, int i)
 	// We can represent a mask in two ways:
 	// 1) as ~0 (all bits will be 1's)
 	// 2) as -1 (all bits are 1's)
+	// So if we move by i to the left, these number of bits will be 0's
 	int mask{ -1 << i};
 	std::cout << std::setw(20) << "Mask: " << std::setw(10) << std::bitset<8>(mask) << std::endl;
 	std::cout << std::setw(20) << "Value: " << std::setw(10) << std::bitset<8>(n) << std::endl;
 	std::cout << std::setw(20) << "Mask & value: " << std::setw(10) << std::bitset<8>(mask & n) << std::endl;
+
+	// Positions from 0 index to index i-1 will cleared
 	n = (n & mask);
 }
 
