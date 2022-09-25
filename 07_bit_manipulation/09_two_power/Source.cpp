3c28b7a045f7c8a00b734cf82ea6c82e600527f1
Michał Maj <michalpmaj@gmail.com>
2022-09-25 17:56:22 +0200

    bitwise power of 2

--- 07_bit_manipulation/09_two_power/Source.cpp
+++ 07_bit_manipulation/09_two_power/Source.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <bitset>
+#include <iomanip>
 
 int main()
 {
 	int n;
 	std::cout << "Enter number to check if its power of 2: ";
 	std::cin >> n;
+
+	// For number, that is power of 2, if we take a number one less than our examined number
+	// and do the AND operation we get a 0 as the result
+	std::cout << "n: " << std::setw(12) << std::bitset<8>(n) << std::endl;
+	std::cout << "n - 1: " << std::bitset<8>(n-1) << std::endl;
+
 
 	if ((n & (n - 1)) == 0)
 		std::cout << "Power of 2!\n";
