d5b2b2b34b920019e98c46928bb5a317d1c333ab
Michał Maj <michalpmaj@gmail.com>
2022-09-25 18:02:03 +0200

    count bits

--- 07_bit_manipulation/10_count_bits/Source.cpp
+++ 07_bit_manipulation/10_count_bits/Source.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
+/**
+ * \brief Count number of 1's bits in given number n.
+ * \param n Number in which we want to count number of 1's bits.
+ * \return Sum of 1's in given number n.
+ */
 int count_bits(int n)
 {
 	int count{ 0 };
 	while(n > 0)
 	{
+		// Calculate using AND to check if bit at given position is 1 or 0
 		int last_bit{ n & 1 };
+
+		// If 0 count will remain the same 
 		count += last_bit;
+
+		// Right shift to check another position.
 		n = n >> 1;
 	}
 
