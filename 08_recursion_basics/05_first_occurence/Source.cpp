9e4a09f011caf3113fc1432ab3cda4e12d697b03
Michał Maj <michalpmaj@gmail.com>
2022-10-04 17:27:42 +0200

    first occurrence in array

--- 08_recursion_basics/05_first_occurence/Source.cpp
+++ 08_recursion_basics/05_first_occurence/Source.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/**
+ * \brief Function to find recursively first occurrence of given value.
+ * \param arr Array that we want to find first occurrence of given value.
+ * \param n Size of the array.
+ * \param key Value that we are looking for.
+ * \return Index of given value or -1 if value is not present in the array.
+ */
 int firstOcc(int arr[], int n, int key)
 {
 	// base case
@@ -7,10 +14,16 @@ int firstOcc(int arr[], int n, int key)
 		return -1;
 
 	// recursive case
+
+	// Check if value is not the first element in the array.
 	if (arr[0] == key)
 		return 0;
 
+	// Recursive search.
+	// Pass the address of next element (arr + 1) - pointers arithmetic
+	// Decrease size of array by 1 at every recursion call.
 	int subIndex = firstOcc(arr + 1, n - 1, key);
+	// If there is a solution we have to add 1 to every recursive call to get final answer.
 	if (subIndex != -1)
 		return subIndex + 1;
 
