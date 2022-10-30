1e8499481238feb7549e6af199a10e56de0b58a7
Michał Maj <michalpmaj@gmail.com>
2022-10-30 16:51:06 +0100

    linked list pop_back

---
+++ 13_linked_list/08_linked_list_pop_back/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "list.h"
+
+
+
+int main()
+{
+	List<int> l;
+	l.push_front(0);
+	l.push_back(1);
+	l.push_back(3);
+	l.push_back(4);
+
+	l.insert(2, 2);
+	l.insert(10, 0);
+
+
+	l.pop_back();
+	//l.print();
+
+
+	return 0;
+}
