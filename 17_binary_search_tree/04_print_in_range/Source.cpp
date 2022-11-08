0e28fce2c74b147618f647b4dcf2d8077286d6d9
Michał Maj <michalpmaj@gmail.com>
2022-11-08 16:33:09 +0100

    print in range

---
+++ 17_binary_search_tree/04_print_in_range/Source.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int key;
+	Node* left;
+	Node* right;
+
+	Node(int key)
+	{
+		this->key = key;
+		left = right = nullptr;
+	}
+
+	~Node()
+	{
+		std::cout << "destructor for key: " << key << std::endl;
+		delete left;
+		delete right;
+	}
+};
+
+Node* insert(Node* root, int key)
+{
+	if (root == nullptr)
+		return new Node(key);
+
+	// rec case
+	if (key < root->key)
+	{
+		root->left = insert(root->left, key);
+	}
+	else
+	{
+		root->right = insert(root->right, key);
+	}
+
+	return root;
+}
+
+bool search(Node* root, int key)
+{
+	if (root == nullptr)
+		return false;
+
+	if (root->key == key)
+		return true;
+
+	if (key < root->key)
+		return search(root->left, key);
+	return search(root->right, key);
+}
+
+void printInOrder(Node* root)
+{
+	if (root == nullptr)
+		return;
+
+	// left, root, right
+	printInOrder(root->left);
+	std::cout << root->key << " ";
+	printInOrder(root->right);
+}
+
+Node* findMin(Node* root)
+{
+	while (root->left != nullptr)
+	{
+		root = root->left;
+	}
+	return root;
+}
+
+// BST Deletion
+Node* remove(Node* root, int key)
+{
+	if (root == nullptr)
+		return nullptr;
+	else if (key < root->key)
+	{
+		root->left = remove(root->left, key);
+	}
+	else if (key > root->key)
+	{
+		root->right = remove(root->right, key);
+	}
+	else
+	{
+		// when the current node match with the key
+		// 1 - No children
+		if (root->left == nullptr and root->right == nullptr)
+		{
+			delete root;
+			root = nullptr;
+		}
+		// 2 - Single child
+		else if (root->left == nullptr)
+		{
+			Node* temp = root;
+			root = root->right;
+			delete temp;
+		}
+		else if (root->right == nullptr)
+		{
+			Node* temp = root;
+			root = root->left;
+			delete temp;
+		}
+		// 3 - Two children
+		else
+		{
+			Node* temp = findMin(root);
+			root->key = temp->key;
+			root->right = remove(root->right, temp->key);
+		}
+	}
+
+	return root;
+}
+
+void printRange(Node* root, int k1, int k2)
+{
+	// base case
+	if(root == nullptr)
+		return;
+
+	// recursive case
+	if(root->key >= k1 and root->key <= k2)
+	{
+		// call on both sides
+		printRange(root->left, k1, k2);
+		std::cout << root->key << " ";
+		printRange(root->right, k1, k2);
+	}
+	else if(root->key > k2)
+	{
+		printRange(root->left, k1, k2);
+	}
+	else
+	{
+		// root->key < k1
+		printRange(root->right, k1, k2);
+	}
+}
+
+int main()
+{
+	Node* root = nullptr;
+	int arr[]{ 8, 3, 10, 1, 6, 14, 4, 7, 13 };
+
+	for (int x : arr)
+		root = insert(root, x);
+
+	printRange(root, 5, 12);
+
+	std::cout << "\n================================================\n\n";
+
+	delete root;
+	root = nullptr;
+
+	return 0;
+}
