158715c8fe80302a108d2609a539c3f874628e3f
Michał Maj <michalpmaj@gmail.com>
2022-11-09 18:15:52 +0100

    priority queue stl custom comparator

---
+++ 18_heaps/04_priority_queue_stl_custom_comparator/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+template <typename T>
+class Compare
+{
+public:
+	bool operator()(T a, T b)
+	{
+		return a < b;
+	}
+};
+
+template<typename Q>
+void printPriorityQueue(Q q)
+{
+	while (!q.empty())
+	{
+		std::cout << q.top() << " ";
+		q.pop();
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int arr[]{ 10, 15, 20, 13, 6, 90 };
+	int n = std::size(arr);
+
+	std::priority_queue<int, std::vector<int>, Compare<int>> heap; // by default it is a max heap, if we want min heap we need to add two more parameters
+	for (const auto& e : arr)
+	{
+		heap.push(e);
+	}
+
+	printPriorityQueue(heap);
+
+
+	return 0;
+}
