db1020875892ac6d9a5cf9801652a5d5122db15a
Michał Maj <michalpmaj@gmail.com>
2022-11-11 18:38:09 +0100

    hashtable searching

---
+++ 19_hashing/05_hashtable_searching/Source.cpp
@@ -0,0 +1,27 @@
+#include "Hashtable.h"
+
+
+int main()
+{
+	Hashtable<int> h;
+
+	h.insert("Mango", 100);
+	h.insert("Apple", 120);
+	h.insert("Banana", 140);
+	h.insert("Orange", 200);
+	h.insert("Kiwi", 120);
+	h.insert("Papaya", 140);
+	h.insert("Litchi", 200);
+
+	h.print();
+
+	std::string fruit{ "Orange" };
+	int* price = h.search(fruit);
+
+	if (price != nullptr)
+		std::cout << "Prince of fruit is " << *price << std::endl;
+	else
+		std::cout << "Fruit not present!" << std::endl;
+
+	return 0;
+}
