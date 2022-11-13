252e1489e844f3df1ca86aae742e90bf03c8769c
Michał Maj <michalpmaj@gmail.com>
2022-11-13 18:48:49 +0100

    trie searching

---
+++ 20_trie_data_structure/03_trie_searching/Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <unordered_map>
+
+class Node
+{
+public:
+	char data;
+	std::unordered_map<char, Node*> m;
+	bool isTerminal;
+
+public:
+	Node(char data) : data(data), isTerminal(false) {}
+};
+
+
+class Trie
+{
+	Node* root;
+
+public:
+	Trie()
+	{
+		root = new Node('\0');
+	}
+
+	~Trie()
+	{
+		delete root;
+	}
+
+	void insert(std::string word)
+	{
+		Node* temp = root;
+
+		for (char ch : word)
+		{
+			if (temp->m.count(ch) == 0)
+			{
+				Node* n = new Node(ch);
+				temp->m[ch] = n;
+				delete n;
+				n = nullptr;
+			}
+			temp = temp->m[ch];
+		}
+		temp->isTerminal = true;
+	}
+
+	bool search(std::string word)
+	{
+		Node* temp = root;
+		for(char ch : word)
+		{
+			if (temp->m.count(ch) == 0)
+				return false;
+			temp = temp->m[ch];
+		}
+		return temp->isTerminal;
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
